ecbc3863f4a06758b78f87313e06514760a5eb11
Maximiliano R Soria <MaximilianoRodrigoSoria@gmail.com>
2023-08-04 13:32:06 -0300

    Se agregan busquedas por id y algorithm, y query por AccountId

--- src/main/java/com/box/documentsuploader/domain/repositories/DocumentRepository.java
+++ src/main/java/com/box/documentsuploader/domain/repositories/DocumentRepository.java
@@ -3,5 +3,14 @@ package com.box.documentsuploader.domain.repositories;
 
 import com.box.documentsuploader.domain.entities.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.Set;
+
 public interface DocumentRepository extends JpaRepository<Document,Long> {
+
+    Optional<Document> findDocumentByAccountIdAndHashSha256OrHashSha512(Long accountId, String sha256,String sha512);
+    Optional<Set<Document>>findDocumentByAccountId(Long accountId);
+
+    Optional<Document> findByAccountIdAndFileName(Long accountId, String filename);
 }
