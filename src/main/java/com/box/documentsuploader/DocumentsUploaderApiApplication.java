96199b148055233c3a55749fb060b1d6921c3b02
Maximiliano R Soria <MaximilianoRodrigoSoria@gmail.com>
2023-08-04 10:11:16 -0300

    Se agrega DocumentsUploaderApiApplication

---
+++ src/main/java/com/box/documentsuploader/DocumentsUploaderApiApplication.java
@@ -0,0 +1,13 @@
+package com.box.documentsuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsUploaderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentsUploaderApiApplication.class, args);
+    }
+
+}
