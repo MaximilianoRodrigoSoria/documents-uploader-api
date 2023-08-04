fd43a5631e845ac1d9cfe8790ef85cb017e8a483
Maximiliano R Soria <MaximilianoRodrigoSoria@gmail.com>
2023-08-04 10:08:48 -0300

    Se agrega Entity Document

---
+++ src/main/java/com/box/documentsuploader/domain/entities/Document.java
@@ -0,0 +1,40 @@
+package com.box.documentsuploader.domain.entities;
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name="documents")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+
+    @Column(name = "hash_sha_256")
+    private String hashSha256;
+
+    @Column(name = "hash_sha_512")
+    private String hashSha512;
+
+    @Column(name = "last_upload")
+    private Date lastUpload;
+
+    @Column(name = "account_id")
+    private Long accountId;
+
+}
+
+
+
