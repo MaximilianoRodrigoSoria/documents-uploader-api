2613ea54146bfd50bf1f20dcbc3a8f5d41c8dfb3
Maximiliano R Soria <MaximilianoRodrigoSoria@gmail.com>
2023-08-04 13:31:09 -0300

    Se agregan endpoints de busqueda

--- src/main/java/com/box/documentsuploader/api/controllers/DocumentController.java
+++ src/main/java/com/box/documentsuploader/api/controllers/DocumentController.java
@@ -2,7 +2,7 @@ package com.box.documentsuploader.api.controllers;
 
 
 import com.box.documentsuploader.domain.dtos.DocumentsResponse;
-import com.box.documentsuploader.services.FilesStorageService;
+import com.box.documentsuploader.services.DocumentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,33 +11,48 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springdoc.api.ErrorMessage;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Objects;
 import java.util.Set;
 
 @RestController
-@RequestMapping(path = "/api/documents/hash")
+@RequestMapping(path = "/api/documents")
 @AllArgsConstructor
 @Tag(name = "Hash Documents")
 public class DocumentController {
 
 
-    private FilesStorageService filesStorageService;
+    private DocumentService filesStorageService;
     @ApiResponse(
             responseCode = "400",
             description = "When the request have a field invalid we response this",
             content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
             }
     )
-    @PostMapping(value = "/", consumes = {"multipart/form-data"})
+    @PostMapping(value = "/hash", consumes = {"multipart/form-data"})
     @Operation(summary = "Upload a single File")
     public ResponseEntity<?> uploadFile(@RequestParam("files") Set<MultipartFile> files, @RequestParam(name = "algorithm") String algorithm) {
         DocumentsResponse fileName= filesStorageService.saveFiles(files, algorithm);
         return ResponseEntity.ok(fileName);
     }
 
-}
+
+    @Operation(summary = "Return a Document")
+    @GetMapping(path = "/{algorithm}")
+    public ResponseEntity<?> findByHash(@PathVariable(name = "algorithm") String algorithm,@RequestParam(name = "hash") String hash) {
+        var response = filesStorageService.getDocumentResponseByHash(algorithm,hash);
+        return Objects.isNull(response) ? ResponseEntity.noContent().build() : ResponseEntity.ok(response);
+    }
+
+
+    @Operation(summary = "Return all documents")
+    @GetMapping(path = "/")
+    public ResponseEntity<?> findAllDocuments() {
+        var response = filesStorageService.findAll();
+        return ResponseEntity.ok(response);
+    }
+
+
+    }
